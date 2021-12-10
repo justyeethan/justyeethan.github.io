4efd1541b3d47168fea181d8c5675247cfcb831f
Ethan <diamondspectreme@gmail.com>
2021-12-09 20:22:00 -0800

    Add more testimonials

--- src/assets/data/testimonials.js
+++ src/assets/data/testimonials.js
@@ -1,19 +1,31 @@
 const testimonials = [
   {
     id: 1,
-    name: 'Gloria Gonzalez-Zapata',
-    title: 'Legislative Intern',
-    org: 'Washington Environmental Council',
-    desc:
-      '"Ethan is wonderful to work with and is exceptional at problem-solving, troubleshooting, and data analysis. He did an excellent job creating an excel database that was easily accessed, managed, and updated for the whole team. Ethan’s innovative and problem-solving skills really benefitted our department operations and gave us more time to be strategic with our work. When we ran into roadblocks whether it was messy data on excel or outdated formulas on excel or computer issues, Ethan always kept a positive mindset and introduced fresh new solutions. Any company would be lucky to have Ethan."',
+    name: "Christian Larson",
+    title: "Senior Engineer",
+    org: "Microsoft",
+    desc: '"Ethan was an incredible intern to work with. His analytical skills and statistical abilities made it easy to understand the trend of data that was happening and he was always the one taking charge of the project. Ethan will be a real asset to any team."',
   },
   {
     id: 2,
-    name: 'Chris Williams',
-    title: 'Senior Engineer',
-    org: 'Microsoft Inc.',
-    desc:
-      '"Ethan was an incredible intern to work with. His analytical skills and statistical abilities made it easy to understand the trend of data that was happening and he was always the one taking charge of the project. Ethan will be a real asset to any team."',
+    name: "Terry Tran",
+    title: "Engineering Project Manager",
+    org: "Apple",
+    desc: '"Ethan is a great team player and he is always willing to fulfill any requirements for the project. He is a hard worker and he is always willing to learn and take criticism. I would recommend Ethan to anyone looking for a team player."',
+  },
+  {
+    id: 3,
+    name: "Sophia Nguyen",
+    title: "Software Engineer Intern",
+    org: "Apple",
+    desc: '"Over the summer, I was able to work with Ethan on a project that was challenging. He was always willing to learn and take criticism, while leading others when they encountered errors in the codebase."',
+  },
+  {
+    id: 4,
+    name: "Gloria Gonzalez-Zapata",
+    title: "Administrative Assistant",
+    org: "University of Washington, Bothell",
+    desc: '"Ethan is wonderful to work with and is exceptional at problem-solving, troubleshooting, and data analysis. He did an excellent job creating an excel database that was easily accessed, managed, and updated for the whole team. Ethan’s innovative and problem-solving skills really benefitted our department operations and gave us more time to be strategic with our work. When we ran into roadblocks whether it was messy data on excel or outdated formulas on excel or computer issues, Ethan always kept a positive mindset and introduced fresh new solutions. Any company would be lucky to have Ethan."',
   },
 ];
 export default testimonials;
